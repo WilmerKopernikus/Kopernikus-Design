e0253e3ada464a460fa28e6f2453ea9c42a1eb2f
WilmerKopernikus <wilmerkopernikus@gmail.com>
2024-10-22 21:54:35 +0800

    que no se pueda hacer scroll mientras se carga

--- loading-script.js
+++ loading-script.js
@@ -6,3 +6,18 @@ window.addEventListener("load", function() {
 
 
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Añadir la clase 'no-scroll' para deshabilitar el scroll
+    document.body.classList.add("no-scroll");
+
+    window.addEventListener("load", function() {
+        // Ocultar el loader y habilitar el scroll una vez que la página esté cargada
+        const loader = document.getElementById("loader");
+        loader.style.display = "none";
+        
+        // Remover la clase 'no-scroll' para habilitar el scroll nuevamente
+        document.body.classList.remove("no-scroll");
+    });
+});
