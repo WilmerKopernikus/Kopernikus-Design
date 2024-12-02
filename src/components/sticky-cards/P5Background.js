ea3f304e34c0582d14a34dbb7165b4045dbd389b
WilmerKopernikus <wilmerkopernikus@gmail.com>
2024-12-02 21:33:44 +0800

    cambio de sketch por uno mas sencillo

--- src/components/sticky-cards/P5Background.js
+++ src/components/sticky-cards/P5Background.js
@@ -1,126 +1,67 @@
 "use client";
+
 import React, { useRef, useEffect } from "react";
 import p5 from "p5";
 
-const P5Background = () => {
+const P5Sketch = () => {
   const sketchRef = useRef();
-  const p5InstanceRef = useRef(null); // Store the p5 instance for global access
 
   useEffect(() => {
     const sketch = (p) => {
-      let objs = [];
-      let colors = ["#ffc400"];
+      let canvas;
 
       p.setup = () => {
-        const canvas = p.createCanvas(p.windowWidth, p.windowHeight);
+        canvas = p.createCanvas(p.windowWidth, p.windowHeight);
         canvas.position(0, 0);
-        canvas.style("z-index", "-1"); // Ensure it's behind other content
+        canvas.style("z-index", "-1");
         canvas.style("position", "fixed");
-        p.rectMode(p.CENTER);
-        addObj();
-        p.background(0);
       };
 
       p.draw = () => {
-        bkgd();
-        for (let i of objs) {
-          i.show();
-          i.move();
-        }
+        p.background("#000000");
 
-        if (p.frameCount % 20 === 0) {
-          addObj();
-        }
+        let ctx = p.drawingContext;
+        let gradient = ctx.createLinearGradient(0, 0, p.width, 0);
+        gradient.addColorStop(0, "#000000");
+        gradient.addColorStop(0.25, "yellow");
+        gradient.addColorStop(0.5, "orange");
+        gradient.addColorStop(0.75, "red");
+        gradient.addColorStop(1, "#000000");
+        ctx.strokeStyle = gradient;
 
-        for (let i = 0; i < objs.length; i++) {
-          if (objs[i].isDead) {
-            objs.splice(i, 1);
-          }
-        }
+        p.strokeWeight(1);
+        p.line(0, p.height / 2, p.width, p.height / 2);
+        p.line(p.width / 2, 0, p.width / 2, p.height);
+
+        let centerX = p.width / 2;
+        let centerY = p.height / 2;
+        let circleX = centerX + (p.mouseX - centerX) / 8;
+        let circleY = centerY + (p.mouseY - centerY) / 4;
+        let circleX2 = centerX - (p.mouseX - centerX) / 8;
+        let circleY2 = centerY - (p.mouseY - centerY) / 4;
+        let circleX3 = centerX - (p.mouseX - centerX) / 8;
+        let circleY3 = centerY + (p.mouseY - centerY) / 4;
+        let circleX4 = centerX + (p.mouseX - centerX) / 8;
+        let circleY4 = centerY - (p.mouseY - centerY) / 4;
+
+        p.noFill();
+        p.ellipse(circleX, circleY, 250, 250);
+        p.ellipse(circleX2, circleY2, 250, 250);
+        p.ellipse(circleX3, circleY3, 250, 250);
+        p.ellipse(circleX4, circleY4, 250, 250);
+        p.ellipse(p.windowWidth / 2, p.windowHeight / 2, 250, 250);
       };
 
-      const addObj = () => {
-        let num = p.int(p.random(1, 25));
-        for (let i = 0; i < num; i++) {
-          objs.push(new OBR(p.random(p.width), p.random(p.height)));
-        }
-      };
-
-      const bkgd = () => {
-        let wid = p.width * 1.2;
-        let c = 90;
-        let w = wid / c;
-
-        p.background("#00000015");
-        for (let i = 0; i < c; i++) {
-          for (let j = 0; j < c; j++) {
-            let x = i * w + w / 2 + (p.width - wid) / 2;
-            let y = j * w + w / 2 + (p.height - wid) / 2;
-            p.noStroke();
-            p.fill(255, 30);
-            p.circle(x, y, p.width * 0.001);
-          }
-        }
-      };
-
-      class OBR {
-        constructor(x, y) {
-          this.x = x;
-          this.y = y;
-          this.d = 0;
-          this.sw = p.width * 0.005;
-          this.isDead = false;
-          this.dStep = p.random(0.2, 0.7);
-          this.swStep = p.random(0.05, 0.009);
-          this.col = p.color(p.random(colors));
-          this.t1 = p.random(23904);
-          this.t2 = p.random(43894);
-          this.xStep = p.random(-1, 1) * 2;
-          this.yStep = p.random(-1, 1) * 2;
-          this.t1Step = p.random(0.1) * p.random();
-          this.t2Step = p.random(0.1) * p.random();
-          this.aStep = p.random(1, 2);
-          this.rnd = p.int(p.random(2));
-          this.alp = 255;
-        }
-
-        show() {
-          if (this.rnd) {
-            p.noFill();
-            p.stroke(this.col);
-            p.strokeWeight(this.sw);
-          } else {
-            p.noStroke();
-            this.col.setAlpha(this.alp);
-            p.fill(this.col);
-          }
-          p.circle(this.x, this.y, this.d);
-        }
-
-        move() {
-          this.d += this.dStep;
-          if (this.rnd) this.sw -= this.swStep;
-          else this.alp -= this.aStep;
-          if (this.sw <= 0 || this.alp <= 0) {
-            this.isDead = true;
-          }
-          this.x += this.xStep * p.sin(this.t1);
-          this.y += this.yStep * p.cos(this.t2);
-          this.t1 += this.t1Step;
-          this.t2 += this.t2Step;
-        }
+      p.windowResized = () => {
+        p.resizeCanvas(p.windowWidth, p.windowHeight);
       };
     };
 
-    p5InstanceRef.current = new p5(sketch, sketchRef.current);
-
-    // Expose pause and resume methods
-    window.pauseP5Sketch = () => p5InstanceRef.current.noLoop();
-    window.resumeP5Sketch = () => p5InstanceRef.current.loop();
+    const p5Instance = new p5(sketch, sketchRef.current);
 
     return () => {
-      if (p5InstanceRef.current) {
-        p5InstanceRef.current.remove();
+      if (p5Instance) {
+        p5Instance.remove();
       }
     };
   }, []);
@@ -128,5 +69,4 @@ const P5Background = () => {
   return <div ref={sketchRef}></div>;
 };
 
-export default P5Background;
-
+export default P5Sketch;
