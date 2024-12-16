52e03bc8e26b44cf63956e85dd074f1de9b0f925
WilmerKopernikus <wilmerkopernikus@gmail.com>
2024-12-16 18:46:19 +0800

    prueba sin javascript

--- video-loading.js
+++ video-loading.js
@@ -1,17 +1,2 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const videos = document.querySelectorAll('.mobile-video');
-  
-    videos.forEach((video) => {
-      // Make sure the video is paused initially (avoid concurrency loading)
-      video.pause();
-  
-      // When the video can play through, start playback
-      video.addEventListener('canplaythrough', () => {
-        video.play().catch(err => {
-          console.warn("Autoplay failed:", err);
-        });
-      });
-    });
-  });
-  
+
   
