51fbdfae22631986b2bd554bef54c921b6e84f47
WilmerKopernikus <wilmerkopernikus@gmail.com>
2025-02-04 19:09:16 +0700

    Prueba de carrusel de imagenes en vez de usar video

--- scripts/sketch_12.js
+++ scripts/sketch_12.js
@@ -49,7 +49,7 @@ function draw() {
 		noStroke();
 		fill(255);
 		textSize(30);
-		text("Drücken Sie, \num Sterne zu erzeugen", centerX, centerY);
+		text("Clicken Sie, \num Sterne zu erzeugen", centerX, centerY);
 		return;
 	}
 	
