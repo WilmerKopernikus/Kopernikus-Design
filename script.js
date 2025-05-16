f35e54add5a359f1ae8f48f44bf89500a9a1b7c8
WilmerKopernikus <wilmerkopernikus@gmail.com>
2025-05-16 12:51:28 +0800

    test con otro gif mas lento

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function isWeChatBrowser() {
 
       if (wechatContainer) {
         wechatContainer.innerHTML = `
-          <img src="imagenes/videos/services_01.gif"
+          <img src="imagenes/videos/services_01b.gif"
                alt="Animated Services GIF"
                id="wechat-services-gif"
                style="width: 100%; height: auto;" />
